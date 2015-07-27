76e13874a4da57adb3f24fad7319f458ece9caf4
Jack Yeh <jack.yeh@gmail.com>
2015-07-27 09:41:18 -0600

    add functionality: orders#index responds to html, json & xml requests

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,22 @@
 class OrdersController < ApplicationController
+
   def index
     @orders = Order.all
+
+    respond_to do |format|
+      format.html { @orders }
+      format.json { render :json => @orders }
+      format.xml { render :xml => @orders }
+    end
   end
 
   def show
     @order = Order.find(params[:id])
+
+    respond_to do |format|
+      format.html { @order }
+      format.json { render :json => @order }
+      format.xml { render :xml => @order }
+    end
   end
 end
